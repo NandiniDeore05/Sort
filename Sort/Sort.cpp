4acc3da367f76b295652051d533fb8348585aeea
Samarthya Jha <samarthya3011@gmail.com>
2020-10-09 11:17:55 +0530

    Added heapsort, mergesort and fixed output

--- Sort/Sort.cpp
+++ Sort/Sort.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 void BubbleSort(int a[],int n)
@@ -126,6 +127,81 @@ void QuickSort(int a[],int l,int h)
     }
 }
 
+void merge(int a[], int l, int mid, int r)
+{
+    int i,j,k;
+    int nl=mid-l+1,nr=r-mid;
+    int left[nl],right[nr];
+    for (i = 0; i < nl; i++) 
+        left[i] = a[l+i]; 
+    for (j = 0; j < nr; j++) 
+        right[j] = a[mid+1+j];
+    i=0;
+    j=0;
+    k=l;
+    while (i < nl && j < nr) 
+    { 
+        if (left[i] <= right[j])
+         { 
+            a[k] = left[i]; 
+            i++; 
+        } 
+        else 
+        { 
+            a[k] = right[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+    while (i < nl)
+    { 
+        a[k] = left[i]; 
+        i++; 
+        k++; 
+    } 
+    while (j < nr) { 
+        a[k] = right[j]; 
+        j++; 
+        k++; 
+    } 
+}
+
+void mergeSort(int a[], int l, int r)
+{
+    if(l<r)
+    {
+        int mid = l+(r-l)/2;
+        mergeSort(a,l,mid);
+        mergeSort(a,mid+1,r);
+        merge(a,l,mid,r);
+    }
+}
+void heapify(int a[], int n, int i) 
+{ 
+    int largest = i;
+    int l = 2*i + 1; 
+    int r = 2*i + 2;
+    if (l < n && a[l] > a[largest]) 
+        largest = l;  
+    if (r < n && a[r] > a[largest]) 
+        largest = r; 
+    if (largest != i) 
+    { 
+        swap(a[i], a[largest]); 
+        heapify(a, n, largest); 
+    } 
+} 
+void heapSort(int a[], int n) 
+{ 
+    for (int i = n / 2 - 1; i >= 0; i--) 
+        heapify(a, n, i);  
+    for (int i=n-1; i>0; i--) 
+    { 
+        swap(a[0], a[i]); 
+        heapify(a, i, 0); 
+    } 
+} 
+  
 int main()
 {
     int a[100];
@@ -139,7 +215,9 @@ int main()
         cout<<"3. SELECTION SORT"<<endl;
         cout<<"4. QUICK SORT"<<endl;
         cout<<"5. SHELL SORT"<<endl;
-        cout<<"6. EXIT"<<endl;
+        cout<<"6. MERGE SORT"<<endl;
+        cout<<"7. HEAP SORT"<<endl;
+        cout<<"8. EXIT"<<endl;
         cout<<"CHOOSE OPTION"<<endl;
         cin>>select;
         switch (select)
@@ -210,17 +288,42 @@ int main()
                 ShellSort(a,n);
                 cout<<"THE SORTED LIST WITH SHELL SORT ===>"<<endl;
                 break;
-            }            
+            }        
+            case 6:
+            {
+                cout<<"ENTER TOTAL NUMBER OF ELEMENTS"<<endl;
+                cin>>n;
+                cout<<"NOW ENTER THE NUMBERS"<<endl;
+                for(int i=0;i<n;i++)
+                {
+                    cin>>a[i];
+                }
+                mergeSort(a,0,n-1);
+                cout<<"THE SORTED LIST WITH MERGE SORT ===>"<<endl;
+                break;    
+            }   
+            case 7:
+            {
+                cout<<"ENTER TOTAL NUMBER OF ELEMENTS"<<endl;
+                cin>>n;
+                cout<<"NOW ENTER THE NUMBERS"<<endl;
+                for(int i=0;i<n;i++)
+                {
+                    cin>>a[i];
+                }
+                heapSort(a,n);
+                cout<<"THE SORTED LIST WITH HEAP SORT ===>"<<endl;
+                break;
+            }
             default:
             {
                 cout<<"INVALID INPUT"<<endl;
                 break;
             }
         }
-    }while(select != 6);
-
-    for(int i=0;i<n;i++)
-    {
-        cout<<a[i]<<endl;
-    }
+        for(int i=0;i<n;i++)
+         {
+            cout<<a[i]<<endl;
+         }
+    }while(select != 8);
 }
